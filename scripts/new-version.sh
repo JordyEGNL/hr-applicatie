4577f798385b8e87e85aa37c13ccd4a99fea03bc
Jordy <65036298+JordyEGNL@users.noreply.github.com>
2024-10-02 10:03:13 +0200

    fix release script

--- scripts/new-version.sh
+++ scripts/new-version.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
+# Opties verwerken
 while getopts 'v:' flag; do
   case "${flag}" in
-  v) VERSION=${OPTARG} ;;
-  *)
-    echo "Invalid args"
-    exit 1
-    ;;
+    v) VERSION=${OPTARG} ;;  # Stel de VERSION variabele in op de opgegeven waarde
+    *)
+      echo "Invalid args"
+      exit 1
+      ;;
   esac
 done
 
-sed -i 's/Version:.*/Version: \"$VERSION\"' src/frontend/login/index.html
-sed -i 's/TAG=.*/TAG=\"$VERSION\"' production.env
+# Controleer of de VERSION variabele is ingesteld
+if [ -z "$VERSION" ]; then
+  echo "Version not provided. Use -v to specify the version."
+  exit 1
+fi
+
+# Update de versie in index.html
+sed -i "s/\(Version: \)[^<]*/\1$VERSION/" src/frontend/login/index.html
+
+# Update de TAG in production.env
+sed -i "s/TAG=.*/TAG=$VERSION/" production.env
